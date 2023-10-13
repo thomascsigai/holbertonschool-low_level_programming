72a11bdca7827a21fd7a9c15a48d84cfead80a54
root <thomascsigai1@gmail.com>
2023-10-13 01:59:44 -0700

    Adding 101-quote.c program

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - Prints the size of various types
+ * main - Prints to the stderr the specified msg
  *
  * Return: 0
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(s, sizeof(s), 1, stderr);
 	return (0);
 }
