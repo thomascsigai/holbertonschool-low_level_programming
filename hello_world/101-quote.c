d0f4896a9ad30cf69d318c406775c850c3e1930f
root <thomascsigai1@gmail.com>
2023-10-13 02:23:58 -0700

    Updating 101-quote.c script

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -9,6 +9,6 @@ int main(void)
 {
 	char s[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(s, sizeof(s), 1, stderr);
+	fwrite(s, sizeof(s)-1, 1, stderr);
 	return (1);
 }
