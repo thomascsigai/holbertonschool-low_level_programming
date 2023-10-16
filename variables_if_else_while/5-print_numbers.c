3df493cb2fbc822efc736af07836b7da83230304
root <thomascsigai1@gmail.com>
2023-10-16 05:10:07 -0700

    Adding 5-print_numbers.c program

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints all single digits number of base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
