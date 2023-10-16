f50d7a3146b1daf258a311d4fe27c3c5f27f0a31
root <thomascsigai1@gmail.com>
2023-10-16 03:25:50 -0700

    Adding 2-print-alphabet.c program

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the last digit of a random number
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 26; i++)
+		putchar(97 + i);
+	putchar('\n');
+	return (0);
+}
