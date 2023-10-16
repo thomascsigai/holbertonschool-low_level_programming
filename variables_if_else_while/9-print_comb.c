99639318e7fe4ecace0110dd67a879181638e7a4
root <thomascsigai1@gmail.com>
2023-10-16 05:53:13 -0700

    Updating 9-print_comb.c to add the new line at the end

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,12 +10,15 @@ int main(void)
 {
 	int i;
 
-	putchar('0');
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(',');
-		putchar(' ');
-		putchar(49 + i);
+		putchar('0' + i);
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+	putchar('\n');
 	return (0);
 }
