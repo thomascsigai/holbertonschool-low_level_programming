429f898c82efed1f6d78e0fa1cca905ef4b70a0f
root <thomascsigai1@gmail.com>
2023-10-16 13:51:02 -0700

    Adding print_alphabet_x10 declaration in main.h and definition in 2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet 10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 26; j++)
+			_putchar('a' + j);
+		_putchar('\n');
+	}
+}
