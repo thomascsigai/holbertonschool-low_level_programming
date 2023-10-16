81313f766b8f7a59cd8f4e9620e86880230aacf6
root <thomascsigai1@gmail.com>
2023-10-16 03:09:38 -0700

    Updating the less than 6 and not 0 condition

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ int main(void)
 		printf("and is greater than 5\n");
 	if (lastDigit == 0)
 		printf("and is 0\n");
-	if (lastDigit < 6)
+	if (lastDigit < 6 && lastDigit != 0)
 		printf("and is less than 6 and not 0\n");
 
 	return (0);
