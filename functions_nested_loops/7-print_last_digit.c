184734d8855a83f5f7d0e3b270b34492f3624c0c
root <thomascsigai1@gmail.com>
2023-10-17 05:15:44 -0700

    Adding print_las_digit() declaration and definition

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Returns the value of the last digit
+ * @n: the number to get the last digit
+ *
+ * Return: the last digit of n
+ */
+int print_last_digit(int n)
+{
+	int lastDigit;
+
+	lastDigit = _abs(n) % 10;
+	_putchar(lastDigit + 48);
+	return (lastDigit);
+}
