bc57522ee178e98a01b4fd4ea8ff282045c1a225
root <thomascsigai1@gmail.com>
2023-10-17 12:08:45 -0700

    Updating print_last_digit() to handle macros

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * print_last_digit - Returns the value of the last digit
@@ -10,9 +11,9 @@ int print_last_digit(int n)
 {
 	int lastDigit;
 
-	if (n < 0)
-		n *= -1;
 	lastDigit = n % 10;
+	if (lastDigit < 0)
+		lastDigit *= -1;
 	_putchar(lastDigit + 48);
 	return (lastDigit);
 }
