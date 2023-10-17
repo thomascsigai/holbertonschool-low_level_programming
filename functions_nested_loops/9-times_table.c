575edffd9a0d65771ceff9553ce504a50ce921a4
root <thomascsigai1@gmail.com>
2023-10-17 06:19:40 -0700

    Adding times_table() declaration and definition

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - Print the 9 times tables
+ *
+ * Return : void
+ */
+void times_table(void)
+{
+	int i, j, num;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0');
+		for (j = 0; j < 10; j++)
+		{
+			num = i * j;
+
+			_putchar(',');
+			_putchar(' ');
+			if (num >= 10)
+				_putchar(num / 10 + 48);
+			else
+				_putchar(' ');
+			_putchar(num % 10 + 48);
+		}
+		_putchar('\n');
+	}
+}
