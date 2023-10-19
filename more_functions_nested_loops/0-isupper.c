84563e7943dcdadfb7b950c9beb50f5b44f98999
root <thomascsigai1@gmail.com>
2023-10-19 08:20:33 -0700

    Adding _isupper() declaration and definition

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase char
+ * @c: the char ascii value to be checked
+ *
+ * Return: 1 if the char is upercase. 0 otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
