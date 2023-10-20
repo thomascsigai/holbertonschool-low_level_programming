94ecbde61f1a1de5abbfd5ec17cd4c4c163c0c26
root <thomascsigai1@gmail.com>
2023-10-20 00:22:11 -0700

    Adding print_most_numbers() declaration and definition

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 to 9 w/out 2 & 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		if (i != 2 || i != 4)
+			_putchar('0' + i);
+	_putchar('\n');
+}
