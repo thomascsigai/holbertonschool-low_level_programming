68afb2c0617a24bd2906d699b83ebb1425ccc104
root <thomascsigai1@gmail.com>
2023-10-21 03:01:29 -0700

    Adding more_numbers() declaration and definition

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers from 0 to 9 w/out 2 & 4
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
