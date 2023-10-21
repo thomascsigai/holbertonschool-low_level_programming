b444fa78405abd0c2e96eeff46e72d58150e6b65
root <thomascsigai1@gmail.com>
2023-10-21 03:56:36 -0700

    Adding fizz_buzz program

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * fizz_buzz - draw fizz buzz
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 99; i++)
+	{
+		if (i % 5 == 0 && i % 3 == 0)
+			printf("FizzBuzz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	}
+	printf("Buzz\n");
+	return (0);
+}
