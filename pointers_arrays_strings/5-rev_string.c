d30cc5ecbdd1ae5cafcf12caa0278d5565c46566
root <thomascsigai1@gmail.com>
2023-10-23 05:43:54 -0700

    Adding rev_string() declaration and definition

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+/**
+ * rev_string - Reverses a string
+ * @s: the string to reverse
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i, sSize = 0;
+
+	while (s[sSize] != '\0')
+		sSize++;
+
+	for (i = 0; i < sSize / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[sSize - 1 - i];
+		s[sSize - 1 - i] = temp;
+	}
+}
