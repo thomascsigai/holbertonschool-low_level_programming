2f1a7caa8a91d8e8d1aea3fdb91b32079dea3bf7
root <thomascsigai1@gmail.com>
2023-10-23 04:53:26 -0700

    Adding print_rev() declaration and definition

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse followed w/ new line
+ * @s: the string to print
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i, sSize = 0;
+
+	while (s[sSize] != '\0')
+		sSize++;
+
+	for (i = sSize; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
