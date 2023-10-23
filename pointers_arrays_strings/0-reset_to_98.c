1f23058d14a3dbdcb0c8bbba155aa64bf483be91
root <thomascsigai1@gmail.com>
2023-10-23 04:05:58 -0700

    Adding reset_to_98() declaration and definition

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - reset the value a pointer points to 98
+ * @n: the pointer pointing to the value to change
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
