9ae5c4d45fd56226e8ccc1695010e73d15251ada
root <thomascsigai1@gmail.com>
2023-10-23 13:44:31 -0700

    Adding _strcpy() declaration and definition

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _strcpy - Copies a string to a buffer
+ * @dest: the buffer
+ * @src: the string to copy
+ *
+ * Return: the pointer to the buffer
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+	char *pDest = dest;
+
+	while (src[i])
+	{
+		pDest[i] = src[i];
+		i++;
+	}
+
+	return (pDest);
+}
