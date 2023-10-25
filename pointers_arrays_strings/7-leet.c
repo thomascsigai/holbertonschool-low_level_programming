4fda01434aef6bbcb188e27ec6920f67c19b5d74
root <thomascsigai1@gmail.com>
2023-10-25 13:37:52 -0700

    Adding leet() declaration and definition

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+/**
+ * leet - encodes a string to 1337
+ * @s: the string to encode
+ *
+ * Return: the string encoded
+ */
+char *leet(char *s)
+{
+	int i = 0, j;
+	char letters[5] = {'a', 'e', 'o', 't', 'l'};
+	char numbers[5] = {'4', '3', '0', '7', '1'};
+
+	while (s[i])
+	{
+		for (j = 0; j < 5; j++)
+			if (s[i] == letters[j])
+				s[i] = numbers[j];
+		i++;
+	}
+
+	return (s);
+}
