272276311fc833c65cdf04b9047e0f9e8560f8eb
thomascsigai <thomascsigai@github.com>
2023-11-29 14:55:28 +0100

    Adding task 5 function declaration and definition

---
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: the head of the list
+ * @index: the index of the node to return
+ *
+ * Return: the nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int nodeCount = 0;
+	dlistint_t *current;
+
+	if (!head)
+		return (NULL);
+
+	current = head;
+
+	while (index != nodeCount && current)
+	{
+		nodeCount++;
+		current = current->next;
+	}
+
+	return (current);
+}
