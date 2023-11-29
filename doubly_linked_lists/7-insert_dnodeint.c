e51556657b4c4eafe98efa920c9e6c06a6b22716
thomascsigai <thomascsigai@github.com>
2023-11-29 15:20:50 +0100

    Adding task 7 function declaration and definition

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - inserts a new node at a given position.
+ * @h: the head of the list
+ * @idx: the index of the list where the new node should be added
+ * @n: the value of the element
+ *
+ * Return: the adress of the new node, NULL otherwise
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int nodeCount = 0;
+	dlistint_t *current, *newNode;
+
+	if (!h)
+		return (NULL);
+
+	current = *h;
+
+	while (current->next && nodeCount + 1 != idx)
+	{
+		nodeCount += 1;
+		current = current->next;
+	}
+
+	if (!current)
+		newNode = NULL;
+	else
+	{
+		newNode = malloc(sizeof(dlistint_t));
+
+		newNode->n = n;
+		newNode->prev = current;
+		newNode->next = current->next;
+
+		current->next = newNode;
+		newNode->next->prev = newNode;
+	}
+
+	return (newNode);
+}
