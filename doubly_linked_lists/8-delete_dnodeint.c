bd32b6bd83e60ec318cf6985a23141e0e46bb204
thomascsigai <thomascsigai@github.com>
2023-11-30 17:21:24 +0100

    Adding task 8 function declaration and definition

---
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes the node at specified index
+ * @head: the head of the list
+ * @index: the index of the node to delete
+ *
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	unsigned int nodeCount = 0;
+	dlistint_t *current;
+
+	if (!head)
+		return (1);
+
+	current = *head;
+
+	while (current)
+	{
+		if (nodeCount == index)
+		{
+			if (current->prev)
+			{
+				current->prev->next = current->next;
+				current->next->prev = current->prev;
+			}
+			else
+			{
+				if (current->next)
+				{
+					current->next->prev = NULL;
+					*head = current->next;
+				}
+				else
+					*head = NULL;
+			}
+
+			free(current);
+
+			return (1);
+		}
+		nodeCount++;
+		current = current->next;
+	}
+
+	return (-1);
+}
