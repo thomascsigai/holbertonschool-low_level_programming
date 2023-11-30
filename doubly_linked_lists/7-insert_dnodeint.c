8701b5b11edce74f32e67a54c9885bf37ba26802
thomascsigai <thomascsigai@github.com>
2023-11-30 10:41:21 +0100

    Handling cases where node is at start/end of list

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,25 @@
 #include "lists.h"
 
 /**
+ * assignNodeValues - assign values on a dlistint node
+ * @node: the node
+ * @next: the next node pointer
+ * @prev: the prev node pointer
+ * @n: the n value
+ *
+ * Return: the adress of the node
+ */
+dlistint_t *assignNodeValues(dlistint_t *node, dlistint_t *next,
+dlistint_t *prev, int n)
+{
+	node->n = n;
+	node->next = next;
+	node->prev = prev;
+
+	return (node);
+}
+
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  * @h: the head of the list
  * @idx: the index of the list where the new node should be added
@@ -11,33 +30,42 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int nodeCount = 0;
-	dlistint_t *current, *newNode;
+	dlistint_t *current, *newNode, *tail;
 
 	if (!h)
 		return (NULL);
-
 	current = *h;
-
-	while (current && nodeCount != idx)
+	while (current && nodeCount++ != idx)
 	{
-		nodeCount += 1;
+		tail = current;
 		current = current->next;
 	}
 
+	newNode = malloc(sizeof(dlistint_t));
+	if (!newNode)
+		return (NULL);
+
 	if (!current)
-		newNode = NULL;
+	{
+		if (nodeCount == idx)
+		{
+			newNode = assignNodeValues(newNode, NULL, tail, n);
+			tail->next = newNode;
+		}
+		else
+		{
+			free(newNode);
+			return (NULL);
+		}
+	}
 	else
 	{
-		newNode = malloc(sizeof(dlistint_t));
-
-		newNode->n = n;
-		newNode->prev = current->prev;
-		newNode->next = current;
-
+		newNode = assignNodeValues(newNode, current, current->prev, n);
+		if (current->prev)
+			current->prev->next = newNode;
+		else
+			*h = newNode;
 		current->prev = newNode;
-		
-		newNode->prev->next = newNode;
 	}
-
 	return (newNode);
 }
